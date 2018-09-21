a6326b94b71a6fd47527ec1dff428a99cf145c5d
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-09-21 00:25:28 -0300

    Fix typos

--- 01-wolf-sheep-cabbage/bfs.py
+++ 01-wolf-sheep-cabbage/bfs.py
@@ -10,15 +10,16 @@ class State:
     def __lt__(self, other):
         return self.cost < other.cost
 
-state = [False, False, False, False]
-
 
 def bfs(state, sucessor_f, final_test_f):
     visited_states = {state}
     heap = [State(state, 0, None)]
     while heap:
         next_expansion = heapq.heappop(heap)
-        print(next_expansion.state)
+        print('State {} with cost {} and parent {}'.format(
+            next_expansion.state,
+            next_expansion.cost,
+            next_expansion.parent and next_expansion.parent.state))
         if final_test_f(next_expansion.state):
             return next_expansion
         next_states = sucessor_f(next_expansion.state)
@@ -29,16 +30,16 @@ def bfs(state, sucessor_f, final_test_f):
 
 
 def sucessor_f(state):
-    boat_move = (not state[0], *state[1:])  # move boat to opossite margin
-    sucessors = []
+    boat_move = (not state[0], *state[1:])  # move boat to opposite margin
+    successors = []
     for i, margin in enumerate(state):
         new_state = list(boat_move)
         new_state[i] = not new_state[i]
         new_state[0] = not state[0]
         if ((new_state[0] == new_state[1] or new_state[1] != new_state[2]) and
             (new_state[0] == new_state[2] or new_state[2] != new_state[3])):
-            sucessors.append(tuple(new_state))
-    return sucessors
+            successors.append(tuple(new_state))
+    return successors
 
 # State: booleans representing whether the boat, wolf, sheep and cabbage are on the right margin
 solution = bfs((False, False, False, False), sucessor_f, all)
